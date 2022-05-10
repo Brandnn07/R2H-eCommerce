9a36011776656804ce957d947217cf0d6b99926b
brandnn07 <brandcarter4@gmail.com>
2022-05-10 19:05:50 -0400

    fixing media query

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
 var cart = document.getElementById("cartNumber");
 var array = [];
 var selection1 = document.getElementById("items1");
@@ -103,17 +100,4 @@ selection12.onclick = function () {
   
 }
 
-btn.onclick = function () {
-  modal.style.display = "block";
-}
-
-span.onclick = function () {
-  modal.style.display = "none";
-}
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
